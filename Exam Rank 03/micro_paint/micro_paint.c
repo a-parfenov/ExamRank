b507e815fdb1072fc1df2fbef973c9b41201ce28
Parfenov <v5pishka@yandex.ru>
2022-03-24 23:28:57 +0300

    Improved code style

--- Exam Rank 03/micro_paint/micro_paint.c
+++ Exam Rank 03/micro_paint/micro_paint.c
@@ -1,82 +1,62 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rectangle.c                                        :+:      :+:    :+:   */
+/*   micro_paint.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aleslie <aleslie@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 22:55:00 by aleslie           #+#    #+#             */
-/*   Updated: 2022/01/31 23:20:10 by aleslie          ###   ########.fr       */
+/*   Updated: 2022/03/24 23:24:30 by aleslie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 int ft_close(FILE *file)
 {
+	write(1, "Error: Operation file corrupted\n", 32);
 	fclose (file);
-	return (1);
+	return 1;
 }
+
 int ft_check(int i, int j, float x, float y, float w, float h)
 {
 	if (i < y || i > y + h || j < x || j > x + w)
-		return (0);
+		return 0;
 	if (i - y < 1 || y + h - i < 1 || j - x < 1 || x + w - j < 1)
-		return (2);
-	return (1);
-}
-int	ft_free(char **area, int height)
-{
-	int i;
-
-	i = -1;
-	while (++i < height)
-		free(area[i]);
-	free(area);
-	return (1);
+		return 2;
+	return 1;
 }
 
-int	ft_error_print(void)
+int main (int argc, char **argv)
 {
-	write(1, "Error: Operation file corrupted\n", 32);
-	return (1);
-}
-
-int	main (int argc, char **argv)
-{
-	FILE	*file;
-	float	x, y, w, h;
-	char	**area, c, back, symb;
-	int		i, j, width, height, arg;
+	FILE *file;
+	char c, symb;
+	float x, y, w, h;
+	int i = -1, j, width, height, arg;
 
 	if (argc != 2)
 		return (write(1, "Error: argument\n", 16) && 1);
 	if (!(file = fopen(argv[1], "r")))
-		return (ft_error_print() && 1);
-	if ((fscanf(file, "%d %d %c\n", &width, &height, &back)) != 3)
-		return (ft_error_print() && ft_close(file));
+		return(write(1, "Error: Operation file corrupted\n", 32) && 1);
+	if (fscanf(file, "%d %d %c\n", &width, &height, &symb) != 3)
+		return (ft_close(file));
 	if (width <= 0 || width > 300 || height <= 0 || height > 300)
-		return (ft_error_print() && ft_close(file));
-	if (!(area = malloc(sizeof(char *) * height)))
 		return (ft_close(file));
 
-	i = -1;
+	char arr[height][width];
 	while (++i < height)
 	{
-		area[i] = malloc(sizeof(char) * width);
-		if (!(area[i]))
-			return (ft_free(area, i) && ft_close(file));
 		j = -1;
 		while (++j < width)
-			area[i][j] = back;
+			arr[i][j] = symb;
 	}
 
 	while ((arg = fscanf(file, "%c %f %f %f %f %c\n", &c, &x, &y, &w, &h, &symb)) == 6)
 	{
 		if ((c != 'r' && c != 'R') || (w <= 0 || h <= 0))
-			return (ft_error_print() && ft_free(area, height) && ft_close(file));
+			return (ft_close(file));
 		i = -1;
 		while (++i < height)
 		{
@@ -84,21 +64,19 @@ int	main (int argc, char **argv)
 			while (++j < width)
 			{
 				if ((ft_check(i, j, x, y, w, h) == 2 && c == 'r') || (ft_check(i, j, x, y, w, h) && c == 'R'))
-						area[i][j] = symb;
+						arr[i][j] = symb;
 			}
 		}
 	}
-
 	if (arg > 0 && arg != 6)
-		return (ft_error_print() && ft_free(area, height) && ft_close(file));
+		return (ft_close(file));
 
 	i = -1;
-	while (++i < height)
-	{
+	while (++i < height) {
 		j = -1;
 		while (++j < width)
-			write(1, &area[i][j], 1);
+			write(1, &arr[i][j], 1);
 		write(1, "\n", 1);
 	}
-	return (ft_free(area, height) && ft_close(file) && 0);
+	return (fclose(file) && 0);
 }
--- Exam Rank 03/mini_paint/mini_paint.c
+++ Exam Rank 03/mini_paint/mini_paint.c
@@ -6,96 +6,71 @@
 /*   By: aleslie <aleslie@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 23:23:02 by aleslie           #+#    #+#             */
-/*   Updated: 2022/02/21 17:13:00 by aleslie          ###   ########.fr       */
+/*   Updated: 2022/03/24 23:23:50 by aleslie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+#include <stdio.h>
 #include <unistd.h>
 
-int	print_error(void)
+int ft_close(FILE *file)
 {
 	write(1, "Error: Operation file corrupted\n", 32);
-	return (1);
-}
-
-int	ft_close(FILE *file)
-{
 	fclose(file);
-	return (1);
+	return 1;
 }
 
-int	ft_free(char **map, int i)
+int main(int argc, char **argv)
 {
-	int	j;
+	FILE *file;
+	char c, symb;
+	float x, y, radius, res;
+	int i = -1, j, height, width, arg;
 
-	j = -1;
-	while (++j < i)
-		free(map[i]);
-	free(map);
-	return (1);
-}
+	if(argc != 2)
+		return(write(1, "Error: argument\n", 16) && 1);
+	if(!(file = fopen(argv[1], "r")))
+		return(write(1, "Error: Operation file corrupted\n", 32) && 1);
+	if(fscanf(file, "%d %d %c\n", &width, &height, &symb) != 3)
+		return(ft_close(file));
+	if(width <= 0 || width > 300 || height <= 0 || height > 300)
+		return(ft_close(file));
 
-int	main(int argc, char **argv)
-{
-	FILE	*file;
-	int		i, j, arg_in_file, widht, height;
-	float	x, y, res, radius;
-	char	**map, back, symb, c;
-
-	if (argc != 2)
-		return (write(1, "Error: argument\n", 16) && 1);
-	if (!(file = fopen(argv[1], "r")))
-		return (print_error() && 1);
-	if ((fscanf(file, "%d %d %c\n", &widht, &height, &back)) != 3)
-		return (print_error() && ft_close(file));
-	if (widht <= 0 || widht > 300 || height <= 0 || height > 300)
-		return (print_error() && ft_close(file));
-
-	map = malloc(sizeof(char *) * height);
-	if (!(map))
-		return (1 && ft_close(file));
-	i = -1;
-	while (++i < height)
-	{
-		map[i] = malloc(sizeof(char) * widht);
-		if (!(map[i]))
-			return (ft_free(map, i) && ft_close(file));
+	char arr[height][width];
+	while(++i < height) {
 		j = -1;
-		while (++j < widht)
-			map[i][j] = back;
+		while(++j < width)
+			arr[i][j] = symb;
 	}
 
-	while ((arg_in_file = fscanf(file, "%c %f %f %f %c\n", &c, &x, &y, &radius, &symb)) == 5)
+	while((arg = fscanf(file, "%c %f %f %f %c\n", &c, &x, &y, &radius, &symb)) == 5)
 	{
-		if ((c != 'c' && c != 'C') || radius <= 0)
-			return (ft_free(map, height) && ft_close(file) && print_error());
+		if((c != 'c' && c != 'C') || radius <= 0)
+			return(ft_close(file));
 		i = -1;
-		while (++i < height)
+		while(++i < height)
 		{
 			j = -1;
-			while (++j < widht)
+			while(++j < width)
 			{
-				res = sqrt(powf(i - y, 2) + powf(j - x, 2));
-				if (res <= radius)
-					if ((radius - res < 1 && c == 'c' ) || c == 'C')
-						map[i][j] = symb;
+				res = sqrt(pow(i - y, 2) + pow(j - x, 2));
+				if(res <= radius)
+					if((radius - res < 1 && c == 'c') || c == 'C')
+						arr[i][j] = symb;
 			}
 		}
 	}
 
-	if (arg_in_file > 0 && arg_in_file != 5)
-		return (ft_free(map, height) && ft_close(file) && print_error());
-	
+	if(arg > 0 && arg != 5)
+		return(ft_close(file));
 	i = -1;
-	while (++i < height)
+	while(++i < height)
 	{
 		j = -1;
-		while (++j < widht)
-			write(1, &map[i][j], 1);
+		while(++j < width)
+			write(1, &arr[i][j], 1);
 		write(1, "\n", 1);
 	}
-	return (ft_free(map, height) && ft_close(file) && 0);
+	return(fclose(file) && 0);
 }
